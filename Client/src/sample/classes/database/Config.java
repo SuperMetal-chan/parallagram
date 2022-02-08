fb7d03e0b357cbce5ed1e50c4baa185ed0e50a0c
Artyom Procenko <supermetalkiller@gmail.com>
2022-02-08 08:39:01 +0200

    fix

--- Client/src/sample/classes/database/Config.java
+++
@@ -1,9 +0,0 @@
-package sample.classes.database;
-
-public class Config {
-    protected String dbHost = "localhost";
-    protected String dbPort = "3306";
-    protected String dbUser = "root";
-    protected String dbPass = "SerMetLiller123";
-    protected String dbName = "messengerschema";
-}
